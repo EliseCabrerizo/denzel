5bd7eb7694dc25aba8bec595e3cb431b0b58ee7d
EliseCabrerizo <elise.cabrerizo@yahoo.fr>
2019-03-22 23:14:03 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -11,15 +11,8 @@ var app = Express();
 var database;
 var collection;
 
-app.listen(process.env.PORT/*3000*/, () => {
-  MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
-      if(error) {
-          throw error;
-      }
-      database = client.db("DenzelFilm");
-      collection = database.collection("Films");
-      console.log("Connected to DenzelFilm/Films !");
-  });
+app.listen(/*process.env.PORT*/3000, () => {
+    MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
         }
@@ -34,39 +27,29 @@ app.get('/', (req, res) => {
   res.end();
 });
 app.get("/movies/populate", async (request, response) => {
-  try {
-    const movies = await imdb(DENZEL_IMDB_ID);
-    collection.insertMany(movies);
-    result = {
-      "total":movies.length
+    try {
+      const movies = await imdb(DENZEL_IMDB_ID);
+      collection.insertMany(movies);
+      result = {
+        "total": movies.length
+      };
+      response.send(result);
+    } catch (e) {
+      console.error(e);
+      return response.status(500).send("Error trying to populate the database");
     }
-    response.send(result);
-    
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-
 });
 app.get("/movies", (request, response) => {
-  collection.find({"metascore" : {$gte :70}}).toArray((error, result) => {
-      if(error) {
-          return response.status(500).send(error);
-      }
-      response.send(result[Math.floor(Math.random()*result.length)]);
-});
-app.get("/movies/:id", (request, response) => {
-  collection.findOne({ "id": (request.params.id) }, (error, result) => {
-      if(error) {
-          return response.status(500).send(error);
-      }
-      response.send(result);
-  });
+    collection.find({"metascore": {$gte: 70}}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result[Math.floor(Math.random() * result.length)]);
+    });
 });
 app.get("/movies/search", (request, response) => {
     var limit = (request.query.limit === undefined ? 5 : parseInt(request.query.limit));
     var metascore = (request.query.metascore === undefined ? 0 : parseInt(request.query.metascore));
-
     collection.find({"metascore": {$gte: metascore}}).limit(limit).toArray((error, result) => {
         if(error) {
             return response.status(500).send(error);
@@ -74,27 +57,32 @@ app.get("/movies/search", (request, response) => {
         response.send(result);
     });
 });
-//curl -X POST -H "Content-Type: application/json" http://localhost:3000/movies/5c868533e72b629f0433421c
-app.post("/movies/:id", (request, response) => {
-  if(request.body.review === undefined || request.body.date === undefined) {
-      return response.status(400).send("You have to specify review and date");
-  }
-  collection.update({"id": request.params.id}, {$set: {"date": request.body.date, "review": request.body.review}}, (error, result) => {
-      if(error) {
-          return response.status(500).send(error);
-      }
-  });
-  collection.findOne({"id": request.params.id}, (error, result) => {
-      if(error) {
-          return response.status(500).send(error);
-      }
-      result = {
-        "_id": result._id
-      };
-      response.send(result);
-  });
+app.get("/movies/:id", (request, response) => {
+    collection.findOne({ "id": request.params.id }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
 });
-
-
+app.post("/movies/:id", (request, response) => {
+    if(request.body.review === undefined || request.body.date === undefined) {
+        return response.status(400).send("You have to specify review and date");
+    }
+    collection.update({"id": request.params.id}, {$set: {"date": request.body.date, "review": request.body.review}}, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+    });
+    collection.findOne({"id": request.params.id}, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        result = {
+          "_id": result._id
+        };
+        response.send(result);
+    });
+});
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
